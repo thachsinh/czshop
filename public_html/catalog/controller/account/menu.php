9dc265bb558b8dfc4868843bcfab9b6c45436591
Thanh Nguyen Van <thanh.nguyen-van.lazada.com>
2015-02-05 21:28:37 +0700

    new file:   public_html/catalog/controller/account/menu.php 	new file:   public_html/catalog/view/theme/default/template/account/account_menu.tpl 	new file:   public_html/static/css/account.css 	new file:   public_html/static/images/footer-mail.png 	new file:   public_html/static/images/footer-phone.png 	new file:   public_html/static/images/header-logout.png 	new file:   public_html/static/images/header-profile.png 	new file:   public_html/static/images/header-user-white.png 	new file:   public_html/static/images/ico-error.png 	new file:   public_html/static/images/ico-success.png

---
+++ public_html/catalog/controller/account/menu.php
@@ -0,0 +1,62 @@
+<?php
+class ControllerAccountMenu extends Controller
+{
+	public function index()
+	{
+		$this->load->model('design/layout');
+		if (!$this->customer->isLogged()) {
+			$this->session->data['redirect'] = $this->url->link('account/account', '', 'SSL');
+			$this->response->redirect($this->url->link('account/login', '', 'SSL'));
+		}
+
+		$this->load->language('account/account');
+
+		$data['text_my_account'] = $this->language->get('text_my_account');
+		$data['text_my_orders'] = $this->language->get('text_my_orders');
+		$data['text_my_newsletter'] = $this->language->get('text_my_newsletter');
+		$data['text_edit'] = $this->language->get('text_edit');
+		$data['text_password'] = $this->language->get('text_password');
+		$data['text_address'] = $this->language->get('text_address');
+		$data['text_wishlist'] = $this->language->get('text_wishlist');
+		$data['text_order'] = $this->language->get('text_order');
+		$data['text_download'] = $this->language->get('text_download');
+		$data['text_reward'] = $this->language->get('text_reward');
+		$data['text_return'] = $this->language->get('text_return');
+		$data['text_transaction'] = $this->language->get('text_transaction');
+		$data['text_newsletter'] = $this->language->get('text_newsletter');
+		$data['text_recurring'] = $this->language->get('text_recurring');
+
+		$data['edit'] = $this->url->link('account/edit', '', 'SSL');
+		$data['password'] = $this->url->link('account/password', '', 'SSL');
+		$data['address'] = $this->url->link('account/address', '', 'SSL');
+		$data['wishlist'] = $this->url->link('account/wishlist');
+		$data['order'] = $this->url->link('account/order', '', 'SSL');
+		$data['download'] = $this->url->link('account/download', '', 'SSL');
+		$data['return'] = $this->url->link('account/return', '', 'SSL');
+		$data['transaction'] = $this->url->link('account/transaction', '', 'SSL');
+		$data['newsletter'] = $this->url->link('account/newsletter', '', 'SSL');
+		$data['recurring'] = $this->url->link('account/recurring', '', 'SSL');
+
+		$this->load->language('account/edit');
+		$data['entry_firstname'] = $this->language->get('entry_firstname');
+		$data['entry_lastname'] = $this->language->get('entry_lastname');
+		$data['entry_email'] = $this->language->get('entry_email');
+		$data['entry_telephone'] = $this->language->get('entry_telephone');
+		$data['entry_fax'] = $this->language->get('entry_fax');
+
+		$this->load->model('account/customer');
+		$data['customer'] = $this->model_account_customer->getCustomer($this->customer->getId());
+
+		if ($this->config->get('reward_status')) {
+			$data['reward'] = $this->url->link('account/reward', '', 'SSL');
+		} else {
+			$data['reward'] = '';
+		}
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/account/account_menu.tpl')) {
+			return $this->load->view($this->config->get('config_template') . '/template/account/account_menu.tpl', $data);
+		} else {
+			return $this->load->view('default/template/account/account_menu.tpl', $data);
+		}
+	}
+}
---
+++ public_html/catalog/view/theme/default/template/account/account_menu.tpl
@@ -0,0 +1,38 @@
+<div class="about">
+    <div class="overlay-on-ajax">
+        <div id="snippet--editing">
+            <dl>
+                <dt><font><font class=""><?php echo $entry_firstname; ?></font></font></dt>
+                <dd class="name"><font><font><?php echo $customer['firstname']; ?></font></font></dd>
+                <dt><font><font><?php echo $entry_telephone; ?></font></font></dt>
+                <dd><font><font><?php echo $customer['telephone']; ?></font></font></dd>
+                <dt><font><font><?php echo $entry_email; ?></font></font></dt>
+                <dd><font><font>khoinv90@gmail.com</font></font></dd>
+            </dl>
+        </div>
+        <p>
+            <a class="ajax" href="<?php echo $edit; ?>"><?php echo $text_edit; ?></a><br/>
+            <a class="ajax" href="<?php echo $password; ?>"><?php echo $text_password; ?></a><br/>
+            <a class="ajax" href="<?php echo $address; ?>"><?php echo $text_address; ?></a><br/>
+            <a class="ajax" href="<?php echo $wishlist; ?>"><?php echo $text_wishlist; ?></a><br/>
+        </p>
+    </div>
+    <div class="overlay-on-ajax billingInfo" id="snippet--changeBillingInfo">
+        <h2><?php echo $text_my_orders; ?></h2>
+        <p style="clear: both; margin-top: 0; padding-top: 0;">
+            <a href="<?php echo $order; ?>"><?php echo $text_order; ?></a><br/>
+            <a href="<?php echo $download; ?>"><?php echo $text_download; ?></a><br/>
+            <?php if ($reward) { ?>
+                <a href="<?php echo $reward; ?>"><?php echo $text_reward; ?></a><br/>
+            <?php } ?>
+            <a href="<?php echo $return; ?>"><?php echo $text_return; ?></a><br/>
+            <a href="<?php echo $transaction; ?>"><?php echo $text_transaction; ?></a><br/>
+            <a href="<?php echo $recurring; ?>"><?php echo $text_recurring; ?></a><br/>
+        </p>
+        <h2><?php echo $text_my_newsletter; ?></h2>
+        <p style="clear: both; margin-top: 0; padding-top: 0;">
+            <a href="<?php echo $newsletter; ?>"><?php echo $text_newsletter; ?></a>
+        </p>
+    </div>
+</div>
+<link rel="stylesheet" type="text/css" href="static/css/account.css">
